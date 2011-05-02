64e3d8efdc2b799d34248700c4ad71b851899ef0
Robert Sesek <rsesek@bluestatic.org>
2011-05-01 22:41:35 -0400

    Add the state machine files and the header stubs

---
+++ Source/State.h
@@ -0,0 +1,49 @@
+/*
+ * MacGDBp
+ * Copyright (c) 2011, Blue Static <http://www.bluestatic.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#import <Cocoa/Cocoa.h>
+
+// A state is a logical unit of work that happens in response to some stimuli.
+// States transition between each other on events.
+@interface State : NSObject {
+ @private
+  StateMachine* machine_;
+  StateEventData* event_;
+}
+
+// Creates a new state with the option of carrying content over through the
+// shared data structure.
+- (id)initWithMachine:(StateMachine*)machine
+      historicalEvent:(StateEventData*)data;
+
+// Creates a machine with an instance of this state as the initial state.
++ (StateMachine*)newAsInitialState;
+
+// Checks whether this state should terminate the execution of the machine,
+// making it the last state.
++ (BOOL)isEndState;
+
+// These methods are called by the machine when it is transitioning to and from
+// a new state. It is ILLEGAL to transition from these methods as a transition
+// is currently in progress when these are called.
+- (void)enterState;
+- (void)exitState;
+
+// Given an event, the state will produce its next state. The machine will
+// transition away from this state to the state returned by this method.
+- (State*)transitionWithEvent:(StateEventData*)event;
+
+@end
---
+++ Source/StateMachine.h
@@ -0,0 +1,56 @@
+/*
+ * MacGDBp
+ * Copyright (c) 2011, Blue Static <http://www.bluestatic.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#import <Cocoa/Cocoa.h>
+
+// A StateMachine manages the transitions from one state to another.
+@interface StateMachine : NSObject {
+ @private
+  State* initialState_;
+  NSMutableDictionary* pendingEvents_;
+  NSMutableArray* states_;
+}
+
+// Creates a new machine with a given initial state. The machine must be started
+// before any work will be done.
+- (id)initWithInitialState:(State*)initialState;
+
+// This starts the state machine by transitioning to the initial state.
+- (void)startMachine;
+
+// Checks if the machine can dispatch the event.
+- (BOOL)wantsEvent:(StateEventData*)event;
+// Causes the machine to transition from the current state to its next state
+// based on the event.
+- (State*)transitionWithEvent:(StateEventData*)event;
+
+// These methods are meant to be used by a State:
+// This causes the machine to perform an actual transition and calls the
+// appropriate methods on each state, as well as updating the internal context.
+- (void)transitionToState:(State*)state;
+// Notifies the machine that a state has finished its work and is waiting for
+// a specific event response to transition.
+- (void)state:(State*)state isWaitingForEvent:(StateEventData*)pendingEvent;
+
+// Queries the machine for the current state.
+- (State*)currentState;
+// Queries the machine for the state it was previously in.
+- (State*)previousState;
+// Checks if the machine is done executing by asking if the current state is an
+// end state.
+- (BOOL)isAtEnd;
+
+@end
